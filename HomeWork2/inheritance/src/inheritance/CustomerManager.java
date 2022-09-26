3dc1b1567c0df3bb7e15e000b11a1da177d7cde1
Nurvural <nurrvural8@gmail.com>
2022-09-26 13:53:23 +0300

    Commit

---
+++ HomeWork2/inheritance/src/inheritance/CustomerManager.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class CustomerManager {
+     public void List() {
+    	 System.out.println("Listelendi");
+     }
+     public void Add() {
+    	 System.out.println("Eklendi");
+     }
+}
