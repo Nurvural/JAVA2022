a5044f7f8282d7809e8b476c389bfc0793c990d5
Nurvural <nurrvural8@gmail.com>
2022-10-07 13:58:53 +0300

    Commit

---
+++ HomeWork3/staticDemo/src/staticDemo/ProductValidator.java
@@ -0,0 +1,32 @@
+package staticDemo;
+
+public class ProductValidator {
+
+	static {
+		System.out.println("Static yapýcý blok çalýþtý");
+	}
+
+	public ProductValidator() {
+		System.out.println("Yapýcý blok çalýþtý");
+	}
+
+	public static boolean isValid(Product product) {
+
+		if (product.price > 0 && !product.name.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public void bisey() {
+
+	}
+	// inner class
+	public static class baskaClass {
+		public static void sil() {
+
+		}
+	}
+}
